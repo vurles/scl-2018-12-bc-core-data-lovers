7bf0bc12b5475f63ecf1cf0843ea1326512f373a
vurles <vurles27@gmail.com>
2019-01-03 16:43:09 -0300

    parte del DOM e intento de  mostrar los nombres de los paises

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,14 @@
 // aqui va todo lo del manejo DOM
+const resultadoPais = nombrePais(dataBank); // guardo en la constante el resultado de mi función nombrePokemones (lo que está retornando)
+//console.log(resultadoNombres);
+
+//llamo al evento click atraves de addEvent..... y le paso mi funcion
+document.getElementById('toShow').addEventListener('click', () => {
+
+  document.getElementById('ejemplo').innerHTML = ''; // limpio el div cada vez que se hace click
+
+  for (let i = 0; i < resultadoPais.length; i++) {
+    document.getElementById('ejemplo').innerHTML += '<p>' + resultadoPais[i] + '</p>'; // imprimo en el HTML cada nombre que está dentro de cada posición del arreglo.
+  }
+
+})
