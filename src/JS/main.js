a7bc2e2044c35589c707d213daed4edfb418f95c
vurles <vurles27@gmail.com>
2019-01-09 13:06:27 -0300

    merged

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,14 +1,17 @@
 // aqui va todo lo del manejo DOM
-const resultadoPais = nombrePais(dataBank); // guardo en la constante el resultado de mi función nombrePokemones (lo que está retornando)
-//console.log(resultadoNombres);
 
-//llamo al evento click atraves de addEvent..... y le paso mi funcion
-document.getElementById('toShow').addEventListener('click', () => {
+const resultadoPais =  document.getElementById("showme");
 
-  document.getElementById('ejemplo').innerHTML = ''; // limpio el div cada vez que se hace click
 
-  for (let i = 0; i < resultadoPais.length; i++) {
-    document.getElementById('ejemplo').innerHTML += '<p>' + resultadoPais[i] + '</p>'; // imprimo en el HTML cada nombre que está dentro de cada posición del arreglo.
-  }
-
-})
+const showData = (WORLDBANK) => {
+  let result = '';
+  //console.log(WORLDBANK)
+     WORLDBANK.forEach(element => {
+        // element --> DATA[i]
+        //console.log(element.house);
+        if (element.data === '') {
+          result = resultadoPais.innerHTML += `
+          <h3>NO aplica</h3>
+          <p>datos por integrar</p>`
+        }
+      })
