8e996d1a002d04d26706d62d8e8f70d8224ea83f
“DanielaEstrada” <dani.strada@gmail.com>
2019-01-10 18:25:12 -0300

    del json a variable. Y ahora imprime los paises

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,26 @@
-function buscar () {
+/*function buscarPais () {
 	let selectCountry = document.getElementById("selectCountry").value;
-	//console.log (selectCountry)
-}
-window.onload = () => {
+	document.getElementById("selectCountry").addEventListener("click",
+		(event) => {
+			event.preventDefault();
+}  
+}*/ 
 
+window.onload = () => {
+	document.getElementById("button_buscar").addEventListener("click",(event) => {
+		let selectCountry = document.getElementById("selectCountry").value;
+		let selectIndicator = document.getElementById("selectIndicator").value;
+		let selectDateOne = document.getElementById("date-one").value;
+		let selectDateTwo = document.getElementById("date-two").value;
+		console.log({selectCountry,selectIndicator,selectDateOne,selectDateTwo});
+
+		
+		console.log(data_bank["PER"]);
+
+		
+	});  
+
+}
 	
 //`src/data.js`:
 //* `filterData(data, condition)`: esta función `filter` o filtrar recibiría la data, y nos retornaría aquellos datos que sí cumplan con la condición.
@@ -22,4 +39,4 @@ window.onload = () => {
 	//	})
 
 
-}
+//}
