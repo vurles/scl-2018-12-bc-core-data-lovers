74e19e00b4dbce360a63abe4e868b9f1bb89e1db
“DanielaEstrada” <dani.strada@gmail.com>
2019-01-10 16:45:30 -0300

    JS

--- JS/main.js
+++ JS/main.js
@@ -5,6 +5,14 @@ function buscar () {
 window.onload = () => {
 
 	
+//`src/data.js`:
+//* `filterData(data, condition)`: esta función `filter` o filtrar recibiría la data, y nos retornaría aquellos datos que sí cumplan con la condición.
+
+//sortData(data, sortBy, sortOrder)`: esta función `sort` u ordenar recibe tres parámetros. 
+//El primer parámetro, `data`, nos entrega los datos.
+//El segundo parámetro, `sortBy`, nos dice con respecto a cuál de los campos de la data se quiere ordenar.
+//El tercer parámetro, `sortOrder`, indica si se quiere ordenar de manera ascendente o descendente.
+	
 
 	//document.getElementById("selectCountry").addEventListener("click",
 	//	(event) => {
