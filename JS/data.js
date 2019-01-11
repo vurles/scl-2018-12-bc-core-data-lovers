be33cda39821f927bedfd09019c6bcb83ab8f169
vurles <vurles27@gmail.com>
2019-01-11 08:25:58 -0300

    trabajo dani

--- JS/data.js
+++ JS/data.js
@@ -1,16 +1,1 @@
-// creo una variable para almacenar mi data
-//const dataBank = WORLDBANK.worldbank;
 
-//const nombrePais = (dataBank) => {
-//  console.log(dataBank);
-//}
-
-const WORLDBANK = [
-{
-"countryName": "Perú",
-"countryCode": "PER",
-"indicatorName": "Empleo de tiempo parcial, mujeres (% del total de mujeres empleadas)",
-"indicatorCode": "SL.TLF.PART.FE.ZS"
-
-}
-] 
